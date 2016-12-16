370077def5e7e00915ed43610ec3434459fba017
Chen Guo <almightygc@gmail.com>
2016-12-16 12:00:44 -0800

    no message

--- bin/package.sh
+++ bin/package.sh
@@ -4,5 +4,5 @@ bin_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $bin_path/..
 
 #Currently at project path
-#mvn package
+mvn package
 tar -czf job-scheduling-dist.tar.gz bin/ target/job-scheduler-1.0-SNAPSHOT-jar-with-dependencies.jar
